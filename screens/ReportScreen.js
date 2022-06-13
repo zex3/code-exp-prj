8afb06279cdaf223476dea97974cfec5a2c6d205
Zi En <kohzien03@gmail.com>
2022-06-13 16:38:13 +0800

    update report screen

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -1,28 +1,51 @@
 import * as React from "react";
-import { useState } from 'react';
-import { Text, View, Button, StyleSheet, Modal, FlatList, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  Modal,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
 
 import FillReportForm from "./FillReportForm/FillReportForm.js";
 
 function ReportScreen() {
   const navigation = useNavigation();
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
   const [report, setReport] = useState([
-    {id: 1, title: "testTitle", description: "testDescriptpion", location: "testLocation"},
-    {id: 2, title: "testTitle2", description: "testDescriptpion2", location: "testLocation2"},
-    {id: 3, title: "testTitle3", description: "testDescriptpion3", location: "testLocation3"}
-  ])
+    {
+      id: 1,
+      title: "testTitle",
+      description: "testDescriptpion",
+      location: "testLocation",
+    },
+    {
+      id: 2,
+      title: "testTitle2",
+      description: "testDescriptpion2",
+      location: "testLocation2",
+    },
+    {
+      id: 3,
+      title: "testTitle3",
+      description: "testDescriptpion3",
+      location: "testLocation3",
+    },
+  ]);
 
-  const addReport = (report) =>{
-      report.id = Math.random().toString();
-      setReport((currentReport)=>{
-        return [report, ...currentReport]
-      })
-      setModalOpen(false)
-  }
+  const addReport = (report) => {
+    report.id = Math.random().toString();
+    setReport((currentReport) => {
+      return [report, ...currentReport];
+    });
+    setModalOpen(false);
+  };
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -33,35 +56,37 @@ function ReportScreen() {
 
       <Modal visible={modalOpen}>
         <View>
-          <MaterialIcons name="close" size={24} onPress={()=>setModalOpen(false)}></MaterialIcons>
+          <MaterialIcons
+            name="close"
+            size={24}
+            onPress={() => setModalOpen(false)}
+          ></MaterialIcons>
           <Text>Modal Open</Text>
-          <FillReportForm addReport={addReport}/>
+          <FillReportForm addReport={addReport} />
         </View>
       </Modal>
 
-      <MaterialIcons name="add" size={24} onPress={() => setModalOpen(true)}></MaterialIcons>
-      <FlatList 
+      <MaterialIcons
+        name="add"
+        size={24}
+        onPress={() => setModalOpen(true)}
+      ></MaterialIcons>
+      <FlatList
         data={report}
-        renderItem={({item})=>(
+        renderItem={({ item }) => (
           <TouchableOpacity>
             <Text> {item.title}</Text>
           </TouchableOpacity>
         )}
-        />
-
-
+      />
     </View>
-    
-
   );
 }
 
 function ReportSecondScreen() {
   return (
-    <View
-      style={styles.GreetingText}
-    >
-      <Text style = {styles.ChangeColor}>Report Form!</Text>
+    <View style={styles.GreetingText}>
+      <Text style={styles.ChangeColor}>Report Form!</Text>
     </View>
   );
 }
@@ -78,13 +103,13 @@ export default function ReportStack() {
 }
 
 const styles = StyleSheet.create({
-  GreetingText : {
+  GreetingText: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
-  ChangeColor : {
-    color: "red"
-}
-})
+  ChangeColor: {
+    color: "red",
+  },
+});
